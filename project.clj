79615309f560f72ede280c9fe16fef41f35829b2
David Goldfarb <deg@degel.com>
2017-08-16 13:30:20 +0300

    Flesh out details of the bug report

--- project.clj
+++ project.clj
@@ -12,11 +12,54 @@
                  [ring "1.4.0"]
 
                  [degree9/firebase-cljs "1.3.0"]
-                 ;; [TODO] I don't know why this next line is needed.  But, without it,
-                 ;; cljsjs/firebase is downloaded ok to ~/.m2, but then I get a warning
+
+                 ;; [BUG ALERT]
+                 ;; The next line should not be needed.
+                 ;;
+                 ;; It shouldn't be here, since firebase-cljs already depends
+                 ;; on cljsjs/firebase. But, without it, Emacs C-c M-J
+                 ;; (M-x cider-jack-in-clojurescript) fails: cljsjs/firebase
+                 ;; is downloaded ok to ~/.m2, but then it gives a warning
                  ;; "Could not find artifact cljsjs:firebase:jar:3.2.1-0 in central
-                 ;;  (http://repo.maven.apache.org/maven2)"
-                 [cljsjs/firebase "3.2.1-0"]]
+                 ;;  (http://repo.maven.apache.org/maven2)" and dies
+                 ;; Strangely, `lein figwheel dev` from a terminal runs fine.
+                 ;;
+                 ;; To duplicate the problem:
+                 ;;
+                 ;; - Clone this repo and comment out [cljsjs/firebase "3.2.1-0"].
+                 ;; - lein clean
+                 ;; - rm ~/.m2/repository/cljsjs/firebase/
+                 ;; - Open emacs buffer on src/cljs/trilystro/events.cljs
+                 ;; - C-c M-J
+                 ;; * FAILURE
+                 ;; * But:
+                 ;; - Open terminal
+                 ;; - lein figwheel dev
+                 ;; * SUCCESS
+                 ;;
+                 ;; There is some discussion of this issue at
+                 ;; https://clojurians-log.clojureverse.org/clojurescript/2017-08-15.html
+                 ;; (search for user "deg") and a gist of the error at
+                 ;; https://gist.github.com/deg/651b7b37ae5943b341e7868fc92590bf
+                 ;;
+                 ;; Background:
+                 ;; - This project is a new, still vanilla lein-based project. It
+                 ;;   was started with
+                 ;;   `lein new re-frame trilystro +cider +test +handler +garden +routes +re-frisk`
+                 ;;   and had only trivial additions.
+                 ;; - degree/firebase-cljs also appears not complicated, but is built
+                 ;;   with boot.
+                 ;; - cljsjs/firebase also appears to be straightforward. And, I
+                 ;;   have duplicated the problem even after substituting different
+                 ;;   versions of it in the firebase-cljs project
+                 ;;
+                 ;; I'm at a loss as to what's gone wrong, but it appears to be somehow
+                 ;; dependent on Cider, and maybe also on lein vs boot interactions.
+                 ;; I'm currently running Cider 20170729.133.
+                 ;;
+                 [cljsjs/firebase "3.2.1-0"]
+
+                 ]
 
   :plugins [[lein-cljsbuild "1.1.4"]
             [lein-garden "0.2.8"]]
