86a6936fcbf48704dfc7a34aca2824c1b8feb8ee
David Goldfarb <deg@degel.com>
2017-09-19 13:58:13 +0300

    Work around CLJS 1.9.908 breakage
    Sep 19, 2017 1:46:59 PM com.google.javascript.jscomp.LoggerErrorManager println
    SEVERE: /home/deg/Documents/git/projects/trilystro/target/cljsbuild-compiler-1/cljs/core.js:3579: ERROR - Parse error. primary expression expected
    case ##Inf:
          ^

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-beta1"]
-                 [org.clojure/clojurescript "1.9.908"]
+                 [org.clojure/clojurescript "1.9.908" :exclusions [[org.clojure/tools.reader]]] [org.clojure/tools.reader"1.1.0"]
                  [reagent "0.7.0"]
                  [re-frame "0.10.1"]
                  [re-frisk "0.5.0"]
