65fb37b9bb4079297a73a284a52acef7013afe45
David Goldfarb <deg@degel.com>
2017-11-04 22:45:30 +0200

    [ch108] Show git version in deployment comment

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # See also https://stackoverflow.com/questions/37667931/how-do-i-deploy-a-single-page-app-written-in-clojurescript-figwheel-to-a-stat
 set -e
 
-deploydir=../trilystro-website
+DEPLOYDIR=../trilystro-website
 
 RED='\033[0;31m'
 NOCOLOR='\033[0m'
@@ -21,7 +21,9 @@ fi
 lein clean
 lein cljsbuild once min || die "Lein cljsbuild failed!"
 
-pushd $deploydir
+GIT_COMMIT=$(git show -s --oneline HEAD)
+
+pushd $DEPLOYDIR
 rm -rf *
 cp -r ../trilystro/resources/public/* .
 cat > CNAME <<EOF
@@ -29,12 +31,12 @@ trilystro.vuagain.com
 EOF
 popd
 
-git describe --always               > $deploydir/git-describe.txt
-git log -1 --format=%cd --date=iso >> $deploydir/git-describe.txt
+git describe --always               > $DEPLOYDIR/git-describe.txt
+git log -1 --format=%cd --date=iso >> $DEPLOYDIR/git-describe.txt
 
-pushd $deploydir
+pushd $DEPLOYDIR
 git add .
-git commit -m "Deploy to GitHub Pages"
+git commit -m "Deploy $GIT_COMMIT"
 git push "git@github.com:deg/trilystro.git" gh-pages:gh-pages
 
 popd
