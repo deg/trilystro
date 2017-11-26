06140bbfbb246e17367cfb2d6bed3321b43816f3
David Goldfarb <deg@degel.com>
2017-11-26 18:18:49 +0200

    Fallback from re-frame-trace to re-frame-frisk. -trace was crashing

--- project.clj
+++ project.clj
@@ -18,7 +18,6 @@
                  [ns-tracker "0.3.1"]
                  [re-frame "0.10.3-alpha1"]
                  [re-frame-utils "0.1.0"]
-                 ;[re-frisk "0.5.2"]
                  [reagent "0.7.0"]
                  [ring "1.6.3"]
                  [secretary "1.2.3"]
@@ -54,7 +53,8 @@
    {:dependencies [[binaryage/devtools "0.9.7"]
                    [figwheel-sidecar "0.5.14"]
                    [com.cemerick/piggieback "0.2.2"]
-                   [day8.re-frame/trace "0.1.13"]
+                   [re-frisk "0.5.2"]
+                   ;[day8.re-frame/trace "0.1.13"]
                    ]
 
     :plugins      [[lein-figwheel "0.5.14"]
@@ -71,8 +71,8 @@
                     :asset-path           "js/compiled/out"
                     :source-map-timestamp true
                     :language-in :ecmascript5
-                    :closure-defines      {"re_frame.trace.trace_enabled_QMARK_" true}
-                    :preloads             [devtools.preload day8.re-frame.trace.preload]
+                    ;:closure-defines      {"re_frame.trace.trace_enabled_QMARK_" true}
+                    :preloads             [devtools.preload #_ day8.re-frame.trace.preload]
                     :external-config      {:devtools/config {:features-to-install :all}}
                     }}
 
